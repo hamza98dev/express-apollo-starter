aa3524b4cd356c51ffb04480d32cd780c452bb1e
aitbahllou ayoub <ayoub.aitbahllou@hotmail.com>
2022-05-07 20:39:19 +0000

    Google auth

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const UserModel = new mongoose.Schema({
-    username:{
+    fullname:{
         type:String,
         required:true
     },
@@ -14,7 +14,6 @@ const UserModel = new mongoose.Schema({
     },
     password:{
         type:String,
-        required:true,
         min: [6, 'password must be at least 6 characters'],
     }
 },{
